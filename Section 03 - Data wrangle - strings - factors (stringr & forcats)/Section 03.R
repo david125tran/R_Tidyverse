bb453c58e54c4e305d9c29c153ce07e20fc21362
David Tran <109535790+david125tran@users.noreply.github.com>
2024-05-27 09:00:30 -0400

    Add files via upload

--- Section 03 - Data wrangle - strings - factors (stringr & forcats)/Section 03.R
+++ Section 03 - Data wrangle - strings - factors (stringr & forcats)/Section 03.R
@@ -6,6 +6,7 @@ graphics.off()
 # install.packages("data.table")
 # install.packages("dplyr")
 # install.packages("feather")
+# install.packages("forcats")
 # install.packages("ggplot2")
 # install.packages("languageserver")
 # install.packages("lintr")
@@ -19,6 +20,7 @@ graphics.off()
 
 # ------------------------------ Load Libraries ------------------------------
 library(dplyr)
+library(forcats)
 library(ggplot2)
 library(stringr)
 library(tibble)
@@ -388,7 +390,6 @@ str_subset(string, "\\n")
 # Different Classes
 string <- c("123abc", "123", ".,?", "ABC", "\nABC", "\tabc")
 
-
 # Match digits
 str_subset(string, "[:digit:]")
 
@@ -407,6 +408,447 @@ str_subset(string, "[:graph:]")
 # Match strings with space characters
 str_subset(string, "[:blank:]")
 
+# ------------------------------ Regular Expressions (regex) - Alternates, Anchors, & Groups ------------------------------
+# Alternates
+#   |           ab|g        Or              Find match "ab" or "g"
+#   []          [abg]       One of          Find match one of "a", "b", or "g"
+#   [^]         [^abg]      Anything but    Find match anything but "a", "b", "g"
+#   [-]         [a-f]       Range           Find match on a range of letters from "a" to "f"
 
+# Anchors
+#   ^           ^s          Start of a string   
+#   $           s$          End of a string 
 
+# Groups
+# (ag|cd)       Group consists of combination of letters: "ag" or "cd"
 
+# Match words that starts with letter "a"
+str_subset(words, "^a")
+
+# Match words that end with letter "a"
+str_subset(words, "a$")
+
+# Match exact word: "actor"
+str_subset(words, "^actor$")
+
+# Match words that start with "af" or "ag" 
+str_subset(words, "^af|^ag")
+
+# Match words containing letters "x", "y", or "z"
+str_subset(words, "[xyz]")
+
+# Match words not containing letters from "a" to "y"
+str_subset(words %>% str_to_lower(), "[^[a-y]]")
+
+# Match country names beginning with "A" or "E"
+str_subset(countries, "^A|^E")
+
+# Match country names ending with letter "a" or "e"
+str_subset(countries, "a$|e$")
+
+# Match sentences that include words: "the", "a", or "an"
+str_subset(sentences, "(\\sthe\\s|\\sa\\s|\\san\\s)")                   # Add "\\s" to only find sentences that have leading and ending white space
+
+# Match words with repeated pair of letters (two letters must be repeated)
+str_subset(words, "(..)\\1")                                            # \1 is a group reference 1st group, double backslash ~ escaping
+
+# ------------------------------ Regular Expressions (regex) - Look Arounds & Quantifiers ------------------------------
+# Look Arounds
+#   (?=)        a(?=c)      Followed by
+#   (?!)        a(?!c)      Not followed by
+#   (?<=)       (?<=b)a     Preceded by
+#   (?<!)       (?<!b)a     Not preceded by
+
+# Quantifiers
+#   ?           a?          Zero or one
+#   *           a*          Zero or more
+#   +           a+          One or more
+#   {n}         a{2}        Exactly n
+#   {n,}        a{2,}       n or more
+#   {n,m}       a{2,3}      Between n and m
+
+# Match words where letter "w" is followed by letter "a"
+str_subset(words, "w(?=a)")
+str_subset(words, "wa")
+
+# Match words where letter "w" is not followed by letter "a"
+str_subset(words, "w(?!a)")
+
+# Match words where letter "a" is preceded by letter "w"
+str_subset(words, "(?<=w)a")
+
+# Match words where letter "a" is not preceded by letter "w"
+str_subset(words, "(?<!w)a")
+
+string <- " .A.AA.AAA.AAAA"
+# Match zero or one "A"
+str_view(string, "A?")
+
+# Match zero or more "A"
+str_view(string, "A*")
+
+# Match one or more "A"
+str_view(string, "A+")
+
+# Match exactly "AA"
+str_view(string, "A{2}")
+
+# Match exactly "AA" or more
+str_view(string, "A{2,}")
+
+# Match between "AA" or "AAA"
+str_view(string, "A{2,3}")
+
+# ------------------------------ Exercise ------------------------------
+# Count the number of words in each sentence
+sentences.df1 <- sentences.df %>%
+    mutate(sentence = str_remove_all(sentence, "[:punct:]"),                    # remove punctuation
+           sentence = str_to_lower(sentence)) %>%                               # convert to lowercase
+    mutate(`nr words` = str_count(string = sentence, pattern = "\\s+") + 1)     # counts number of spaces between words, "+ 1" added to get number of words
+
+# Get the frequencies of word count
+sentences.df1 %>% count(`nr words`)
+# View(sentences.df1)
+
+# Countries with more than 3 words in a country name
+countries.df %>%
+    mutate(`nr words` = str_count(country, "\\s+") + 1) %>%
+    filter(`nr words` > 3)
+
+# ------------------------------ Factors ------------------------------
+# Create a factor variables
+df <- mpg %>%
+    mutate_at(.vars = c("manufacturer", "model", "trans", "class"),
+              .funs = as_factor)
+
+# Check factor levels
+df$manufacturer %>% levels()
+
+# Count factor values - fct_count()
+df %>% 
+    .$manufacturer %>% 
+    fct_count()
+
+                    # # A tibble: 15 × 2
+                    #    f              n
+                    #    <fct>      <int>
+                    #  1 audi          18
+                    #  2 chevrolet     19
+                    #  3 dodge         37
+                    #  4 ford          25
+                    #  5 honda          9
+                    #  6 hyundai       14
+                    #  7 jeep           8
+                    #  8 land rover     4
+                    #  9 lincoln        3
+                    # 10 mercury        4
+                    # 11 nissan        13
+                    # 12 pontiac        5
+                    # 13 subaru        14
+                    # 14 toyota        34
+                    # 15 volkswagen    27
+
+# Visualize frequencies 
+df %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# Get unique values - fct_unique()
+df %>% 
+    .$manufacturer %>%
+    fct_unique()
+                    #  [1] audi       chevrolet  dodge      ford       honda      hyundai
+                    #  [7] jeep       land rover lincoln    mercury    nissan     pontiac
+                    # [13] subaru     toyota     volkswagen
+
+# ------------------------------ Factors - Combine and Order ------------------------------
+df <- mpg %>%
+    mutate_at(.vars = c("manufacturer", "model", "trans", "class"),
+              .funs = as_factor)
+
+# Combine and order levels - fct_c()
+# First split cars into 2 data frames
+manufacturers <- df %>%
+    .$manufacturer %>%
+    fct_unique() %>%
+    as.character()
+
+                    #  [1] "audi"       "chevrolet"  "dodge"      "ford"       "honda"
+                    #  [6] "hyundai"    "jeep"       "land rover" "lincoln"    "mercury"
+                    # [11] "nissan"     "pontiac"    "subaru"     "toyota"     "volkswagen"
+
+df1 <- df %>%
+    filter(manufacturer %in% manufacturers[1:8])
+
+df2 <- df %>%
+    filter(manufacturer %in% manufacturers[1:8])
+
+# Extract only factor vector
+f1 <- df1 %>%
+    pull(manufacturer)
+
+f2 <- df2 %>%
+    pull(manufacturer)
+
+levels(f1)
+
+                    #  [1] "audi"       "chevrolet"  "dodge"      "ford"       "honda"
+                    #  [6] "hyundai"    "jeep"       "land rover" "lincoln"    "mercury"
+                    # [11] "nissan"     "pontiac"    "subaru"     "toyota"     "volkswagen"
+
+levels(f2)
+
+                    #  [1] "audi"       "chevrolet"  "dodge"      "ford"       "honda"
+                    #  [6] "hyundai"    "jeep"       "land rover" "lincoln"    "mercury"
+                    # [11] "nissan"     "pontiac"    "subaru"     "toyota"     "volkswagen"
+
+# Combine factors
+fct_c(f1, f2)
+
+# Manually reorder levels - fct_relevel()
+# First let's randomly shuffle the levels
+set.seed(478)
+manufacturers.rnd <- sample(manufacturers, size = length(manufacturers), replace = F)
+
+                    #  [1] "ford"       "land rover" "lincoln"    "toyota"     "volkswagen"
+                    #  [6] "mercury"    "pontiac"    "hyundai"    "nissan"     "audi"
+                    # [11] "subaru"     "dodge"      "honda"      "chevrolet"  "jeep"
+
+# Count frequencies & create another bar plot with the manually reordered levels
+df %>%
+    mutate(manufacturer = fct_relevel(manufacturer, manufacturers.rnd)) %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# Define levels by frequencies - fct_infreq()
+# Order manufacturers based on car count
+df %>%
+    mutate(manufacturer = fct_infreq(manufacturer)) %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# Define levels by the order that they first appear in the data - fct_inorder()
+# Order manufacturers based on first appearance in the data
+df %>%
+    mutate(manufacturer = fct_inorder(manufacturer)) %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# Define levels by the order that they last appear in the data - fct_rev()
+# Order manufacturers in reverse order of appearance in the data
+df %>%
+    mutate(manufacturer = fct_rev(manufacturer)) %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# Get the reverse frequency count - fct_infreq & fct_rev combined
+# Order manufactures based on their frequency in reverse
+df %>%
+    mutate(manufacturer = fct_infreq(manufacturer),
+           manufacturer = fct_rev(manufacturer)) %>%
+    count(manufacturer) %>%
+    ggplot(aes(x = manufacturer, 
+               y = n)) + 
+    geom_col()
+
+# ------------------------------ Factors - Change Values, Add or Drop Levels ------------------------------
+# Change levels - fct_recode()
+# First pull levels and add country of origin column
+df %>% 
+    pull(manufacturer) %>%
+    fct_count()
+
+                    # # A tibble: 15 × 2
+                    #    f              n
+                    #    <fct>      <int>
+                    #  1 audi          18
+                    #  2 chevrolet     19
+                    #  3 dodge         37
+                    #  4 ford          25
+                    #  5 honda          9
+                    #  6 hyundai       14
+                    #  7 jeep           8
+                    #  8 land rover     4
+                    #  9 lincoln        3
+                    # 10 mercury        4
+                    # 11 nissan        13
+                    # 12 pontiac        5
+                    # 13 subaru        14
+                    # 14 toyota        34
+                    # 15 volkswagen    27
+
+levels.country <- tribble(
+    ~company, ~country, 
+    "audi", "Germany",
+    "chevrolet", "USA",
+    "dodge", "USA",
+    "ford", "USA",
+    "honda", "Japan",
+    "hyundai", "South Korea",
+    "jeep", "USA",
+    "land rover", "England",
+    "lincoln", "USA",
+    "mercury", "USA",
+    "nissan", "Japan",
+    "pontiac", "USA",
+    "subaru", "Japan",
+    "toyota", "Japan",
+    "volkswagen", "Germany"
+)
+
+levels.country
+
+                    # # A tibble: 15 × 2
+                    #    company    country
+                    #    <chr>      <chr>
+                    #  1 audi       Germany
+                    #  2 chevrolet  USA
+                    #  3 dodge      USA
+                    #  4 ford       USA
+                    #  5 honda      Japan
+                    #  6 hyundai    South Korea
+                    #  7 jeep       USA
+                    #  8 land rover England
+                    #  9 lincoln    USA
+                    # 10 mercury    USA
+                    # 11 nissan     Japan
+                    # 12 pontiac    USA
+                    # 13 subaru     Japan
+                    # 14 toyota     Japan
+                    # 15 volkswagen Germany
+
+# Prepare pairs for recoding factor levels
+levels.country %>%
+    mutate(recode = str_c(country, " = ", "'", company, "'", sep = "")) %>%
+    pull(recode) %>%
+    str_c(.,collapse = ", ")
+
+# [1] "Germany = 'audi', USA = 'chevrolet', USA = 'dodge', USA = 'ford', Japan = 'honda', South Korea = 'hyundai', USA = 'jeep', England = 'land rover', USA = 'lincoln', USA = 'mercury', Japan = 'nissan', USA = 'pontiac', Japan = 'subaru', Japan = 'toyota', Germany = 'volkswagen'"
+
+# Recoding 'manufacturer' column to country name
+df.recode <- df %>%
+    mutate(manufacturer = fct_recode(manufacturer, 
+                                     Germany = 'audi', 
+                                     USA = 'chevrolet', 
+                                     USA = 'dodge', 
+                                     USA = 'ford',
+                                     Japan = 'honda', 
+                                     `South Korea` = 'hyundai', 
+                                     USA = 'jeep', 
+                                     England = 'land rover', 
+                                     USA = 'lincoln', 
+                                     USA = 'mercury', 
+                                     Japan = 'nissan', 
+                                     USA = 'pontiac', 
+                                     Japan = 'subaru', 
+                                     Japan = 'toyota', 
+                                     Germany = 'volkswagen'))
+df.recode
+
+                    #    manufacturer model      displ  year   cyl trans drv     cty   hwy fl    class
+                    #    <fct>        <fct>      <dbl> <int> <int> <fct> <chr> <int> <int> <chr> <fct>
+                    #  1 Germany      a4           1.8  1999     4 auto… f        18    29 p     comp…
+                    #  2 Germany      a4           1.8  1999     4 manu… f        21    29 p     comp…
+                    #  3 Germany      a4           2    2008     4 manu… f        20    31 p     comp…
+                    #  4 Germany      a4           2    2008     4 auto… f        21    30 p     comp…
+                    #  5 Germany      a4           2.8  1999     6 auto… f        16    26 p     comp…
+                    #  6 Germany      a4           2.8  1999     6 manu… f        18    26 p     comp…
+                    #  7 Germany      a4           3.1  2008     6 auto… f        18    27 p     comp…
+                    #  8 Germany      a4 quattro   1.8  1999     4 manu… 4        18    26 p     comp…
+                    #  9 Germany      a4 quattro   1.8  1999     4 auto… 4        16    25 p     comp…
+                    # 10 Germany      a4 quattro   2    2008     4 manu… 4        20    28 p     comp…
+
+df.recode %>%
+    count(manufacturer)
+
+                    # # A tibble: 5 × 2
+                    #   manufacturer     n
+                    #   <fct>        <int>
+                    # 1 Germany         45
+                    # 2 USA            101
+                    # 3 Japan           70
+                    # 4 South Korea     14
+                    # 5 England          4
+
+# Collapse levels - fct_collapse()
+# Keep only USA companies, others are collapsed
+non.US.manufacturers <- levels.country %>%
+    filter(country != "USA") %>%
+    pull(company)
+
+                    # [1] "audi"       "honda"      "hyundai"    "land rover" "nissan"
+                    # [6] "subaru"     "toyota"     "volkswagen"
+
+df.collapse <- df %>%
+    mutate(manufacturer = fct_collapse(manufacturer, `non US` = non.US.manufacturers))
+
+df.collapse %>%
+    count(manufacturer)
+
+                    # # A tibble: 8 × 2
+                    #   manufacturer     n
+                    #   <fct>        <int>
+                    # 1 non US         133
+                    # 2 chevrolet       19
+                    # 3 dodge           37
+                    # 4 ford            25
+                    # 5 jeep             8
+                    # 6 lincoln          3
+                    # 7 mercury          4
+                    # 8 pontiac          5
+
+# Put levels into other groups - fct_other()
+# All `non US` companies are put into `Other`
+df.other <- df %>%
+    mutate(manufacturer = fct_other(manufacturer, drop = non.US.manufacturers))
+
+df.other %>%
+    count(manufacturer)
+
+                    # # A tibble: 8 × 2
+                    #   manufacturer     n
+                    #   <fct>        <int>
+                    # 1 chevrolet       19
+                    # 2 dodge           37
+                    # 3 ford            25
+                    # 4 jeep             8
+                    # 5 lincoln          3
+                    # 6 mercury          4
+                    # 7 pontiac          5
+                    # 8 Other          133
+
+# Drop levels - fct_drop()
+# Drop `Other` level.  First filter out rows with `Other`
+df.drop <- df.other %>%
+    filter(manufacturer != "Other")
+
+# Check levels - "Other" still present
+df.drop %>% 
+    pull(manufacturer) %>%
+    fct_unique()
+
+# Drop levels
+df.drop <- df.drop %>%
+    mutate(manufacturer = fct_drop(manufacturer))
+
+# Add new levels - fct_expand()
+df.expand <- df %>%
+    mutate(manufacturer = fct_expand(manufacturer, c("Ferrari", "Lamborghini")))
+
+df.expand %>%
+    pull(manufacturer) %>%
+    fct_unique()
+
+                    #  [1] audi       chevrolet  dodge      ford       honda      hyundai
+                    #  [7] jeep       land rover lincoln    mercury    nissan     pontiac
+                    # [13] subaru     toyota     volkswagen Ferrari    Lamborghini
