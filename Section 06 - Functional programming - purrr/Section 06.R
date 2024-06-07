8d457b059bb32071224f9521360d058c3c44f874
David Tran <109535790+david125tran@users.noreply.github.com>
2024-06-07 09:59:57 -0400

    Add files via upload

--- Section 06 - Functional programming - purrr/Section 06.R
+++ Section 06 - Functional programming - purrr/Section 06.R
@@ -250,3 +250,235 @@ list <- list(list(1:3, 4:6, 7:9),
 
 # Extract third element from each sub-list and put the extraction inside of a list
 list %>% map(3)
+
+# ------------------------------ Map Over More Than One Argument: map2() ------------------------------
+?rnorm 
+
+# distribution parameters
+mu <- c(0, -4, 5, 8)
+sig <- c(1, 2, 1, 3)
+
+data <- map2(.x = mu, 
+             .y = sig, 
+             .f = rnorm, 
+             n = 1000) %>%
+    enframe() %>%
+    mutate(name = paste0("norm", 1:4)) %>%
+    unnest(cols = c(value))
+
+data %>% count(name)
+
+# Visualize
+data %>%
+    ggplot(aes(x = value,
+               color = name,
+               fill = name)) +
+    geom_density(size = 1.3,
+                 alpha = 0.25) +
+    theme_minimal()
+
+# ------------------------------ Map Over More Than One Argument: pmap() ------------------------------
+# Distribution parameters
+n <- c(100, 100, 1500, 10000)
+mu <- c(0, -4, 5, 8)
+sig <- c(1, 2, 1, 3)
+
+args <- list(n, mu, sig)
+
+# Generate data
+pmap(.l = args, .f = rnorm) %>% str()
+
+# Put inside of a tibble
+args <- tibble(mean = mu,
+               sd = sig,
+               n = n)
+
+# Generate data
+data <- args %>%
+    pmap(.l = ., .f = rnorm) %>%
+    enframe() %>%
+    mutate(name = paste0("norm", 1:4)) %>%
+    unnest(cols = c(value))
+
+data %>% count(name)
+
+# Visualize
+# Visualize
+data %>%
+    ggplot(aes(x = value,
+               color = name,
+               fill = name)) +
+    geom_density(size = 1.3,
+                 alpha = 0.25) +
+    theme_minimal()
+
+# ------------------------------ Map Over More Than One Argument: walk() ------------------------------
+# Simple example with print
+l <- list(1:3, c(2, 4, 6))
+map(l, print)
+
+walk(l, print)
+
+# Create plots
+plots <- mpg %>%
+    split(.$manufacturer) %>%
+    map(~ggplot(., aes(displ, hwy)) + geom_point() + ggtitle(paste0(.$manufacturer)))
+
+# Create directory for plots
+setwd("C:/Users/Laptop/Desktop/R - Tidyverse/Section 06 - Functional programming - purrr")
+path <- "C:/Users/Laptop/Desktop/R - Tidyverse/Section 06 - Functional programming - purrr/data/pwalk_plots"
+
+if (dir.exists(path) == FALSE) {
+    dir.create(path, recursive = TRUE)
+
+}
+
+# Export plots
+list(str_c(path, "/", names(plots), ".pdf"), plots) %>%
+    pwalk(., ggsave)
+
+# ------------------------------ Work with Lists ------------------------------
+# Create a list
+set.seed(123)
+
+# Single variables
+l1 <- T
+l2 <- F
+s1 <- words %>%
+    sample(1)
+s2 <- words %>%
+    sample(1)
+n1 <- runif(1, 0, 1000)
+n2 <- runif(1, 0, 1000)
+
+# Vectors
+vec.l1 <- sample(c(T, F), size = round(runif(1, 1, 100)), replace = T)
+vec.l2 <- sample(c(T, F), size = round(runif(1, 1, 100)), replace = T)
+vec.s1 <- words %>% sample(size = round(runif(1, 1, 100)), replace = T)
+vec.s2 <- words %>% sample(size = round(runif(1, 1, 100)), replace = T)
+vec.n1 <- runif(1, 0, 1000) %>% sample(size = round(runif(1, 1, 100)), replace = T)
+vec.n2 <- runif(1, 0, 1000) %>% sample(size = round(runif(1, 1, 100)), replace = T)
+
+# Tables
+t1 <- mpg
+t2 <- diamonds %>% sample_n(size = 500)
+
+# Lists
+list1 <- list(a = 1, b = "b", vec = 1:10)
+list2 <- list(vec = seq(0, 10, 0.05), words = words[1:10])
+
+# Put everything into one list
+list <- list(l1 = l1,
+             l2 = l2,
+             s1 = s1,
+             s2 = s2,
+             n1 = n1,
+             n2 = n2,
+             vec.l1 = vec.l1,
+             vec.l2 = vec.l2,
+             vec.s1 = vec.s1,
+             vec.s2 = vec.s2,
+             vec.n1 = vec.n1,
+             vec.n2 = vec.n2,
+             t1 = t1,
+             t2 = t2)
+
+list %>% str()
+
+# Reshuffle elements
+list.shuffled <- list[sample(1:length(list), size = length(list), replace = F)] # nolint
+
+list <- list.shuffled
+rm(list.shuffled)
+
+# ------------------------------ Work with Lists: pluck() ------------------------------
+# Get list elements at index 1
+pluck(list, 1)
+
+# Get elements from "vec.l1"
+pluck(list, "vec.l1")
+
+# ------------------------------ Work with Lists: keep() ------------------------------
+list %>% map(class) %>% unlist()
+
+# Keep only character objects
+keep(list, is.character)
+
+# Keep only logical objects
+keep(list, is.logical)
+
+# ------------------------------ Work with Lists: discard() ------------------------------
+# Discard logical objects
+discard(list, is.logical)
+
+# ------------------------------ Work with Lists: flatten() ------------------------------
+# Flatten our list
+list.f <- flatten(list)
+
+# ------------------------------ Work with Lists: transpose() ------------------------------
+list.t <- transpose(list)
+list.t %>% str()
+
+# ------------------------------ Summarise & Join Lists: every() and sum() ------------------------------
+# Every list is logical object
+every(list, is.logical)
+
+# Some lists are logical object
+some(list, is.logical)
+
+# ------------------------------ Summarise & Join Lists: has_element() ------------------------------
+# Does the list have an element
+has_element(list, mpg)
+
+# ------------------------------ Summarise & Join Lists: detect() and detect_index() ------------------------------
+# Find indices with numeric objects
+detect(list, is.numeric)
+
+# Find the first index with numeric object
+detect_index(list, is.numeric)
+
+# ------------------------------ Summarise & Join Lists: vec_depth() ------------------------------
+# Check the depth of your list
+vec_depth(list1)
+
+# ------------------------------ Summarise & Join Lists: append() and prepend() ------------------------------
+# Create smaller lists
+sublist1 <- list(pluck(list, 1), pluck(list, 2))
+sublist2 <- list(pluck(list, 3), pluck(list, 4))
+
+# Append list
+append(sublist1, sublist2)
+
+# Pre-pend list
+prepend(sublist1, sublist2)
+
+# ------------------------------ Summarise & Join Lists: splice() ------------------------------
+splice(l1, vec.l1, list1)
+
+# ------------------------------ Transform Lists: modify() ------------------------------
+modify(list, class)
+
+modify_if(list, is.data.frame, nrow)
+
+modify_at(list, "vec.s2", length)
+
+# ------------------------------ Transform Lists: reduce() ------------------------------
+list.c <- list(sample(letters, 17),
+               sample(letters, 17),
+               sample(letters, 17),
+               sample(letters, 17))
+
+list.n <- list(runif(n=10),
+               runif(n=10),
+               runif(n=10),
+               runif(n=10))
+
+# Intersection of letters
+reduce(list.c, intersect)
+
+# Calculate a cumulative sum numeric vectors
+reduce(list.n, sum)
+
+# ------------------------------ Transform Lists: accumulate ------------------------------
+accumulate(list.c, intersect)
+
